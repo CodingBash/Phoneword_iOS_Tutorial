e0df0571c73fc033ac84d6a2b983f729a2ddf67b
Basheer Becerra <bbecerr@outlook.com>
2020-03-30 20:29:46 -0500

    Draft of tutorial

--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -13,7 +13,38 @@ namespace Phoneword_iOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            string translatedNumber = "";
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs E) =>
+            {
+                translatedNumber = PhoneTranslator.ToNumber(PhoneNumberText.Text);
+
+                PhoneNumberText.ResignFirstResponder();
+
+                if (translatedNumber == "")
+                {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                var url = new NSUrl("tel: " + translatedNumber);
+
+                if (!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+                }
+            };
         }
 
         public override void DidReceiveMemoryWarning()
